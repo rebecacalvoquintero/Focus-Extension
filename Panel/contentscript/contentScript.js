e7f3639217f393796acb3663a2aaa9eaaf1e93ff
rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2019-04-01 22:02:39 +0100

    logging counts

---
+++ Panel/contentscript/contentScript.js
@@ -0,0 +1,33 @@
+var timeoutID;
+
+function setup() {
+    this.addEventListener('mousemove', resetTimer, false);
+    this.addEventListener('mousedown', resetTimer, false);
+    this.addEventListener('keypress', resetTimer, false);
+    this.addEventListener('scroll', resetTimer, false);
+    this.addEventListener('wheel', resetTimer, false);
+    this.addEventListener('touchmove', resetTimer, false);
+    this.addEventListener('pointermove', resetTimer, false);
+
+    startTimer();
+}
+
+setup();
+
+function startTimer() {
+    timeoutID = window.setTimeout(goInactive, 5000);
+}
+
+function resetTimer(e) {
+    window.clearTimeout(timeoutID);
+    goActive();
+}
+
+function goInactive() {
+    chrome.runtime.sendMessage({ userActive: false });
+}
+
+function goActive() {
+    chrome.runtime.sendMessage({ userActive: true });
+    startTimer();
+}
--- Panel/index.js
+++ Panel/index.js
@@ -3,6 +3,9 @@ let count = {};
 const extractHostname = (url) => {
   let hostname;
   //find & remove protocol (http, ftp, etc.) and get hostname
+  if (!url) {
+    return '';
+  }
   if (url.indexOf("//") > -1) {
     hostname = url.split('/')[2];
   } else {
@@ -16,10 +19,16 @@ const extractHostname = (url) => {
   return hostname;
 }
 
+// user goes to a new tab
+// and can see the previous tabs stored with the seconds
+const getAnotherTabs = () => {
+
+}
+
 const trackWebsites = () => {
-  const {
-    host
-  } = window.location;
+
+  const { host } = window.location;
+
   chrome.tabs.query({
     "active": true,
     "lastFocusedWindow": true
@@ -28,15 +37,17 @@ const trackWebsites = () => {
     if (tabs[0]) {
       const host = extractHostname(tabs[0].url);
       count[host] = (count[host] || 0) + 1;
-      let site = location.hostname;
-      let countString = Object.keys(count).map(key => `${key}: ${count[key]} seconds`);
-      document.getElementById("log").innerHTML = countString;
 
+      chrome.storage.sync.set({count}, () => {
+        console.log(`Value is set to ${count[host]}`);
+      });
+
+      chrome.storage.sync.get(['count'], ({count}) => {
+        let countString = Object.keys(count).map(key => `${key}: ${count[host]} seconds`);
+        document.getElementById("log").innerHTML = countString;
+      });
     }
   })
 
 }
-
-
-
 setInterval(trackWebsites, 1000);
