09bf05c9f508630e115f1a17fa907544034b2326
rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2019-04-02 11:09:07 +0100

    fixed linter errors

--- Panel/index.js
+++ Panel/index.js
@@ -7,31 +7,30 @@ getCountFromStorage((count) => {
   count = count || {};
 
   const trackWebsites = () => {
-
     const { host } = window.location;
 
-    chrome.tabs.query({"active": true}, (tabs) => {
-      console.log("tabs", tabs);
+    chrome.tabs.query({active: true }, (tabs) => {
+      console.log('tabs', tabs);
       // let url = tabs[0].url;
       if (tabs[0]) {
         const host = extractHostname(tabs[0].url);
         count[host] = (count[host] || 0) + 1;
 
-        chrome.storage.sync.set({count}, () => {
+        chrome.storage.sync.set({count }, () => {
           console.log(`Value is set to ${count[host]}`);
         });
       }
-    })
-  }
+    });
+  };
 
   const updateUi = () => {
     getCountFromStorage(count => {
-      let countString = Object.keys(count)
+      const countString = Object.keys(count)
         .map(key => `<div>${key}: ${count[key]}</div>`)
         .join('\n');
       document.getElementById("log").innerHTML = countString;
     });
-  }
+  };
 
   // on a loop update website count` with current website
   setInterval(trackWebsites, 1000);
@@ -45,7 +44,7 @@ const extractHostname = (url) => {
   if (!url) {
     return '';
   }
-  if (url.indexOf("//") > -1) {
+  if (url.indexOf('//') > -1) {
     hostname = url.split('/')[2];
   } else {
     hostname = url.split('/')[0];
@@ -56,4 +55,4 @@ const extractHostname = (url) => {
   hostname = hostname.split('?')[0];
 
   return hostname;
-}
+};
